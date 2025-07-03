156d383b1afc8287683676593fbbc30f601f7150
Franco Ruggieri <101886749+FranRuggi@users.noreply.github.com>
2025-07-03 12:38:15 -0300

    Add files via upload

---
+++ src/nodo/Nodo.java
@@ -0,0 +1,31 @@
+package nodo;
+
+import java.util.Objects;
+
+import general.Coordenada;
+
+public abstract class Nodo {
+
+	public double distanciaA(Nodo otro) {
+		return this.getUbicacion().distanciaA(otro.getUbicacion());
+	}
+
+	public abstract Coordenada getUbicacion();
+
+	public abstract boolean esRobopuerto();
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Nodo nodo = (Nodo) o;
+		return this.getUbicacion().equals(nodo.getUbicacion());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getUbicacion());
+	}
+}
